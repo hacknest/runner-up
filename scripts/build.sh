058cb36ef50ab69a59a578625beb9da612f65ccb
tony-dinh <tonyd@sfu.ca>
2017-03-18 17:18:14 -0700

    💥 fix path for env.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-./$ROOT_DIR/scripts/env.sh
+pushd $ROOT_DIR
+./scripts/env.sh
+popd
 webpack --config $ROOT_DIR/webpack.config.js
 sass $ROOT_DIR/src/app/styles/styles.scss:$ROOT_DIR/src/client/styles.css
 
