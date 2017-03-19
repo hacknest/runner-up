7e69a624d8b4e1d4412c89f1a72d7f3ac497727b
Andy Liu <mingchia.andy.liu@gmail.com>
2017-03-18 20:44:21 -0700

    Fix broken interger id

--- src/app/js/containers/home/index.js
+++ src/app/js/containers/home/index.js
@@ -19,8 +19,8 @@ class Home extends React.Component {
         const routes = [{id:123, name:'ABBY GRIND', difficulty:5, time:'12h30m15s',distance:10, elevation:500},
                 {id:456,name:'BRIDAL VEIL FALLS', difficulty:5, time:'2h30m15s',distance:10, elevation:500},
                 {id:789,name:'BRUNSWICK POINT', difficulty:5, time:'2h30m15s',distance:10, elevation:500},
-                {id:001,name:'BRUNSWICK POINT', difficulty:5, time:'2h30m15s',distance:10, elevation:500},
-                {id:007,name:'BRUNSWICK POINT', difficulty:5, time:'2h30m15s',distance:10, elevation:500}
+                {id:125,name:'BRUNSWICK POINT', difficulty:5, time:'2h30m15s',distance:10, elevation:500},
+                {id:999,name:'BRUNSWICK POINT', difficulty:5, time:'2h30m15s',distance:10, elevation:500}
         ];
         return (
             <div class='t-page u-flex-column u--center-cross'>
